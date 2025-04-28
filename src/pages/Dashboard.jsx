c3965deebaa6406e9724be6c0a7e71bc05774468
Mariete_Colequete <34687013+MARO-445@users.noreply.github.com>
2025-04-28 05:41:28 +0200

    Added the ai-model-predictions element

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,14 +2,27 @@ import { SensorOverview } from '../components/SensorOverview';
 import Name_card from '../components/Name-card';
 import Sensor_card from '../components/Sensor-cards'
 import Notification_centre from '../components/Notification-centre'
+import { AIModelPredictions } from '../components/AIModelPredictions';
 
 function Dashboard() {
+  /* 
+   * Input data for the AI model predictions
+   * Right now, this is hardcoded
+   * TODO: Fetch this data from the backend
+   */
+  const plantMetrics = [
+    { name: 'Temperature', unit: 'ºC', value: 28, optimal: 25 },
+    { name: 'Light Intensity', unit: 'lux', value: 18000, optimal: 20000 },
+    { name: 'Humidity', unit: '%', value: 45, optimal: 60 },
+  ];
+  
   return (
     <div className="p-4">
       <Name_card />
       <Sensor_card />
       <SensorOverview />
       <Notification_centre />
+      <AIModelPredictions metrics={plantMetrics} />
     </div>
   );
 }

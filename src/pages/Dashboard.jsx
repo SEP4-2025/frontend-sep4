91edc8e77889f094b744c0c3bdebb821f5a2d235
Mariete_Colequete <34687013+MARO-445@users.noreply.github.com>
2025-04-29 18:36:46 +0200

    fixed not used constants

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ import { SensorOverview }    from '../components/SensorOverview';
 import Notification_centre   from '../components/Notification-centre';
 import { AIModelPredictions } from '../components/AIModelPredictions';
 
-const dummyMetrics = [
+const _dummyMetrics = [
   { name: 'Temperature',     unit: 'ºC',  value: 23,   optimal: 25 },
   { name: 'Light Intensity', unit: 'lux', value: 15000, optimal: 20000 },
   { name: 'Humidity',        unit: '%',   value: 45,   optimal: 60 },
@@ -16,13 +16,9 @@ function Dashboard() {
   const [metrics, setMetrics] = useState([]);
 
   useEffect(() => {
-    if (false) { // Comment this line to use the real data
-      setMetrics(dummyMetrics); // Comment this line to use the real data
-    } else {
       compileDashboardData({ untilDate: '2025-04-29', notificationLimit: 5 })
         .then(({ metrics }) => setMetrics(metrics))
         .catch(console.error);
-    }
   }, []);
 
   return (

964823203962e38216874e46ade746e4a7b4115e
Mateusz Samborski <msamborski04@gmail.com>
2025-05-07 17:33:34 +0200

    fixed errors with lint

--- src/utils/dataCompiler.js
+++ src/utils/dataCompiler.js
@@ -1,15 +1,12 @@
 import {
-  fetchSensorsData,
   fetchGrenhouseDataByGardenerId,
-  getSensorDataLastest,
-  getSensorData
+  getSensorDataLastest
 
 } from '../api';
 
 export async function compileDashboardData(gardenerId) {
   // Geting the data from the database
   const greenhouseData = await fetchGrenhouseDataByGardenerId(gardenerId)
-  const sensorData = await getSensorData('all');
 
   // Filtering the data to get the light sensor data
   let lightSensorData = await getSensorDataLastest('light')

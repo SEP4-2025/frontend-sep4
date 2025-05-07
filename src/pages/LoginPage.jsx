2e296cd4a2b3bd935fede404a5f53642f74a31b4
Richardzik <kubrama04@gmail.com>
2025-05-07 16:36:46 +0200

    Renamed the email to username in login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,13 +5,13 @@ import loginBackground from '../assets/login_background.jpeg';
 import logo from '../assets/GrowMate_Logo_Transparent.png';
 
 function LoginPage({ setIsAuthenticated }) {
-    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
     const handleLogin = async () => {
-        if (!email || !password) {
-            alert("Please fill in both email and password.");
+        if (!username || !password) {
+            alert("Please fill in both username and password.");
             return;
         }
 
@@ -19,7 +19,7 @@ function LoginPage({ setIsAuthenticated }) {
             const response = await fetch("https://webapi-service-68779328892.europe-north2.run.app/auth/login", {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ username: email, password }) // assuming email = username
+                body: JSON.stringify({ username: username, password })
             });
 
             if (response.ok) {
@@ -47,13 +47,13 @@ function LoginPage({ setIsAuthenticated }) {
                     <h1 className='Jacques-Francois text-2xl'>Grow smarter, not harder</h1>
                 </div>
                 <div className='flex flex-col bg-white gap-3 p-6 rounded-lg'>
-                    <p>Email</p>
+                    <p>Username</p>
                     <input
                         type="text"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
                         className="border p-2 rounded"
-                        placeholder="Insert Email"
+                        placeholder="Insert Username"
                     />
                     <p>Password</p>
                     <input

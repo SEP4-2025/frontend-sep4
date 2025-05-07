59e86a124d01037ab51f3f876df0b05a17620c28
Mariete_Colequete <34687013+MARO-445@users.noreply.github.com>
2025-05-07 17:20:12 +0200

    'added-sensor-functions'

--- src/api/index.js
+++ src/api/index.js
@@ -29,3 +29,74 @@ export async function fetchSensorsData(){
   return res.json();
 }
 
+/*
+* 
+*   NEW API ENDPOINTS - Mariete
+* 
+*/
+
+/*
+*   GetSensorData(type)
+*   INPUT: 
+*   type (string) - type of sensor data to fetch || possible values: "all", "temperature", "humidity", "light", "soilMoisture"
+*   RETURNS:
+*   Array of sensor data objects || each object contains the following properties:
+*     id (int) - id of the sensor data
+*     date (string) - date of the sensor data
+*     value (int) - value of the sensor data
+*     sensorId (int) - id of the sensor
+*       1 - temperature
+*       2 - humidity
+*       3 - light
+*       4 - soilMoisture
+*/
+
+export async function getSensorData(type) {
+  // Validate the type parameter
+  if (!['all', 'temperature', 'humidity', 'light', 'soilMoisture'].includes(type)) {
+    throw new Error(`Invalid sensor type: ${type}`);
+  }
+
+  // Transform type to lowercase
+  type = type.toLowerCase();
+
+  // Translate type into int
+  let typeId = '';
+  if (type === 'temperature') typeId = 'sensor/1';
+  else if (type === 'humidity') typeId = 'sensor/2';
+  else if (type === 'light') typeId = 'sensor/3';
+  else if (type === 'soilmoisture') typeId = 'sensor/4';
+
+  // Fetch data from the API
+  const res = await fetch(`${BASE_URL}/SensorReading/${typeId}`);
+  if (!res.ok) throw new Error(`Failed to load sensor data for type ${type}, which is ${typeId}`);
+  return res.json();
+}
+
+/*
+*   GetSensorDataLastest(type)
+*   INPUT:
+*   type (string) - type of sensor data to fetch || possible values: "all", "temperature", "humidity", "light", "soilMoisture"
+*   RETURNS:
+*   Lastest sensor data object || the object contains the following properties:
+*     id (int) - id of the sensor data
+*     date (string) - date of the sensor data
+*     value (int) - value of the sensor data
+*     sensorId (int) - id of the sensor
+*       1 - temperature
+*       2 - humidity
+*       3 - light
+*       4 - soilMoisture
+*/
+
+export async function getSensorDataLastest(type) {
+  const allData = await getSensorData(type)
+
+  // Get the latest data from the response's array (last element)
+  const data = await allData;
+  const latestData = data[data.length - 1];
+  if (!latestData) throw new Error(`No data found for type ${type}`);
+
+  return latestData;
+}
+
--- src/utils/dataCompiler.js
+++ src/utils/dataCompiler.js
@@ -1,17 +1,18 @@
 import {
   fetchSensorsData,
-  fetchGrenhouseDataByGardenerId
+  fetchGrenhouseDataByGardenerId,
+  getSensorDataLastest,
+  getSensorData
 
 } from '../api';
 
 export async function compileDashboardData(gardenerId) {
   // Geting the data from the database
   const greenhouseData = await fetchGrenhouseDataByGardenerId(gardenerId)
-  const sensorData = await fetchSensorsData();
+  const sensorData = await getSensorData('all');
 
   // Filtering the data to get the light sensor data
-  let lightSensorData = sensorData.filter(item => item.sensorId == 1); 
-  lightSensorData  = lightSensorData [0];
+  let lightSensorData = await getSensorDataLastest('light')
   
 
   return {

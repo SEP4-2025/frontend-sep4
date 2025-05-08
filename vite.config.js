9ce2f04bdfe0d70bde9a7964b45fdbb419062dcf
Mateusz Samborski <msamborski04@gmail.com>
2025-05-08 16:29:31 +0200

    Added Flowbite library to the project

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'; // Import svgr
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()], // Add svgr back
+  plugins: [react(), tailwindcss(), svgr(), flowbiteReact()], // Add svgr back
   server: {
     proxy: {
       '/api': {

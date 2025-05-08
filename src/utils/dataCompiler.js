579c4eb748efe33205d2dbfe30824d52ab2c8c95
Mariete_Colequete <34687013+MARO-445@users.noreply.github.com>
2025-05-08 12:26:19 +0200

    fixed-compatibility-with-main

--- src/utils/dataCompiler.js
+++ src/utils/dataCompiler.js
@@ -1,12 +1,17 @@
 import {
+  fetchSensorsData,
   fetchGrenhouseDataByGardenerId,
-  getSensorDataLastest
+  getSensorDataLastest,
+  getSensorData
 
 } from '../api';
 
 export async function compileDashboardData(gardenerId) {
   // Geting the data from the database
   const greenhouseData = await fetchGrenhouseDataByGardenerId(gardenerId)
+  const sensorData = await getSensorData('all');
+
+  // Filtering the data to get the light sensor data
   const lightSensorData = await getSensorDataLastest('light')
   const temperatureSensorData = await getSensorDataLastest('temperature')
   const humiditySensorData = await getSensorDataLastest('humidity')

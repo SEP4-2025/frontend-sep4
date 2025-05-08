354b7d1592dec7bed45bed1071e245a04b7fbd48
Richardzik <kubrama04@gmail.com>
2025-05-08 17:17:16 +0200

    Added functionality for sensor cards to display the difference

--- src/components/Sensor-cards.jsx
+++ src/components/Sensor-cards.jsx
@@ -4,7 +4,10 @@ import waterLevelIcon from '../assets/hugeicons--humidity.svg';
 import lightIntensityIcon from '../assets/entypo--light-up.svg';
 import soilMoistureIcon from '../assets/soil-moisture-icon.svg';
 
-function Sensor_card({ lightSensorData, temperatureSensorData, humiditySensorData, soilMoistureSensorData }) {  
+function Sensor_card({ lightSensorData, temperatureSensorData, humiditySensorData, soilMoistureSensorData,
+    lightSensorDataAverageToday, temperatureSensorDataAverageToday, humiditySensorDataAverageToday, soilMoistureSensorDataAverageToday,
+    lightSensorDataAverageYesterday, temperatureSensorDataAverageYesterday, humiditySensorDataAverageYesterday, soilMoistureSensorDataAverageYesterday }) {
+
     return (
         <div className='flex flex-row justify-evenly '>
             <div className='border-1 border-gray-500 rounded-xl pl-2 bg-navbar-color pb-2 pt-1 pr-1 w-1/6'>
@@ -12,24 +15,36 @@ function Sensor_card({ lightSensorData, temperatureSensorData, humiditySensorDat
                     <p className='Manrope text-xl'>Temperature</p>
                     <img src={temperatureIcon} className="ml-auto" alt="temperature icon" width="23" height="2" />
                 </div>
-                <p className='Manrope text-l font-bold'>{temperatureSensorData.value}°C</p>
-                <p className='Manrope text-xs text-gray-400'>-2.2°C from yesterday</p>
+                <p className='Manrope text-l font-bold'>{temperatureSensorData.value.toFixed(1)}°C</p>
+                <p className='Manrope text-xs text-gray-400'>
+                    {temperatureSensorDataAverageToday && temperatureSensorDataAverageYesterday ?
+                        `${(temperatureSensorDataAverageToday > temperatureSensorDataAverageYesterday ? '+' : '-')}${(temperatureSensorDataAverageToday - temperatureSensorDataAverageYesterday).toFixed(1)}°C from yesterday` :
+                        'No comparison data'}
+                </p>
             </div >
             <div className='border-1 border-gray-500 rounded-xl pl-2 bg-navbar-color pb-2 pt-1 pr-1 w-1/6'>
                 <div className='flex flex-row'>
                     <p className='Manrope text-xl'>Humidity</p>
                     <img src={humidityIcon} className="ml-auto" alt="humidity icon" width="23" height="2" />
                 </div>
-                <p className='Manrope text-l font-bold'>{humiditySensorData.value}%</p>
-                <p className='Manrope text-xs text-gray-400'>+4% from yesterday</p>
+                <p className='Manrope text-l font-bold'>{humiditySensorData.value.toFixed(1)}%</p>
+                <p className='Manrope text-xs text-gray-400'>
+                    {humiditySensorDataAverageToday && humiditySensorDataAverageYesterday ?
+                        `${(humiditySensorDataAverageToday > humiditySensorDataAverageYesterday ? '+' : '-')}${(humiditySensorDataAverageToday - humiditySensorDataAverageYesterday).toFixed(1)}% from yesterday` :
+                        'No comparison data'}
+                </p>
             </div>
             <div className='border-1 border-gray-500 rounded-xl pl-2 bg-navbar-color pb-2 pt-1 pr-1 w-1/6'>
                 <div className='flex flex-row'>
                     <p className='Manrope text-xl'>Soil Moisture</p>
                     <img src={soilMoistureIcon} className="ml-auto" alt="soil moisture icon" width="23" height="2" />
                 </div>
-                <p className='Manrope text-l font-bold'>{soilMoistureSensorData.value}%</p>
-                <p className='Manrope text-xs text-gray-400'>+4% from yesterday</p>
+                <p className='Manrope text-l font-bold'>{soilMoistureSensorData.value.toFixed(1)}%</p>
+                <p className='Manrope text-xs text-gray-400'>
+                    {soilMoistureSensorDataAverageToday && soilMoistureSensorDataAverageYesterday ?
+                        `${(soilMoistureSensorDataAverageToday > soilMoistureSensorDataAverageYesterday ? '+' : '-')}${(soilMoistureSensorDataAverageToday- soilMoistureSensorDataAverageYesterday).toFixed(1)}% from yesterday` :
+                        'No comparison data'}
+                </p>
             </div>
             <div className='border-1 border-gray-500 rounded-xl pl-2 bg-navbar-color pb-2 pt-1 pr-1 w-1/6'>
                 <div className='flex flex-row'>
@@ -44,8 +59,12 @@ function Sensor_card({ lightSensorData, temperatureSensorData, humiditySensorDat
                     <p className='Manrope text-xl'>Light intensity</p>
                     <img src={lightIntensityIcon} className="ml-auto" alt="light intensity icon" width="23" height="2" />
                 </div>
-                <p className='Manrope text-l font-bold'>{lightSensorData.value} lux</p>
-                <p className='Manrope text-xs text-gray-400'>Optimal Range</p>
+                <p className='Manrope text-l font-bold'>{lightSensorData.value.toFixed(0)} lux</p>
+                <p className='Manrope text-xs text-gray-400'>
+                    {lightSensorDataAverageToday && lightSensorDataAverageYesterday ?
+                        `${(lightSensorDataAverageToday > lightSensorDataAverageYesterday ? '+' : '-')}${(lightSensorDataAverageToday- lightSensorDataAverageYesterday).toFixed(0)}lux from yesterday` :
+                        'No comparison data'}
+                </p>
             </div>
         </div>
     );
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,17 @@
 import { useState, useEffect } from 'react';
 import { compileDashboardData } from '../utils/dataCompiler';
-import Name_card             from '../components/Name-card';
-import Sensor_card           from '../components/Sensor-cards';
-import { SensorOverview }    from '../components/SensorOverview';
-import Notification_centre   from '../components/Notification-centre';
+import Name_card from '../components/Name-card';
+import Sensor_card from '../components/Sensor-cards';
+import { SensorOverview } from '../components/SensorOverview';
+import Notification_centre from '../components/Notification-centre';
 import { AIModelPredictions } from '../components/AIModelPredictions';
-import ClockCard              from '../components/Clock-card';
+import ClockCard from '../components/Clock-card';
 import LoadingScreen from '../components/Loading-screen';
 
 const _dummyMetrics = [
-  { name: 'Temperature',     unit: 'ºC',  value: 23,   optimal: 25 },
+  { name: 'Temperature', unit: 'ºC', value: 23, optimal: 25 },
   { name: 'Light Intensity', unit: 'lux', value: 15000, optimal: 20000 },
-  { name: 'Humidity',        unit: '%',   value: 45,   optimal: 60 },
+  { name: 'Humidity', unit: '%', value: 45, optimal: 60 },
 ];
 
 function Dashboard() {
@@ -20,39 +20,72 @@ function Dashboard() {
   const [humiditySensorData, setHumiditySensorData] = useState([]);
   const [soilMoistureSensorData, setSoilMoistureSensorData] = useState([]);
   const [greenhouseData, setGreenhouseData] = useState([]);
+  const [lightSensorDataAverageToday, setLightSensorDataAverageToday] = useState([]);
+  const [temperatureSensorDataAverageToday, setTemperatureSensorDataAverageToday] = useState([]);
+  const [humiditySensorDataAverageToday, setHumiditySensorDataAverageToday] = useState([]);
+  const [soilMoistureSensorDataAverageToday, setSoilMoistureSensorDataAverageToday] = useState([]);
+  const [lightSensorDataAverageYesterday, setLightSensorDataAverageYesterday] = useState([]);
+  const [temperatureSensorDataAverageYesterday, setTemperatureSensorDataAverageYesterday] = useState([]);
+  const [humiditySensorDataAverageYesterday, setHumiditySensorDataAverageYesterday] = useState([]);
+  const [soilMoistureSensorDataAverageYesterday, setSoilMoistureSensorDataAverageYesterday] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const gardenerId = 1; // TODO: We will need to pass the information about the gardenerId from the login page to the dashboard page, for now it is hardcoded
     compileDashboardData(gardenerId)
       .then((data) => {
+        // Set the current sensor data
         setlightSensorData(data.lightSensorData);
         setTemperatureSensorData(data.temperatureSensorData);
         setHumiditySensorData(data.humiditySensorData);
         setSoilMoistureSensorData(data.soilMoistureSensorData);
+        // Set the greenhouse data
         setGreenhouseData(data.greenhouseData);
+        // Set the average sensor data today
+        setLightSensorDataAverageToday(data.lightSensorDataAverageToday);
+        setTemperatureSensorDataAverageToday(data.temperatureSensorDataAverageToday);
+        setHumiditySensorDataAverageToday(data.humiditySensorDataAverageToday);
+        setSoilMoistureSensorDataAverageToday(data.soilMoistureSensorDataAverageToday);
+        // Set the average sensor data yesterday
+        setLightSensorDataAverageYesterday(data.lightSensorDataAverageYesterday);
+        setTemperatureSensorDataAverageYesterday(data.temperatureSensorDataAverageYesterday);
+        setHumiditySensorDataAverageYesterday(data.humiditySensorDataAverageYesterday);
+        setSoilMoistureSensorDataAverageYesterday(data.soilMoistureSensorDataAverageYesterday);
+
         setIsLoading(false);
       })
       .catch((error) => {
         console.error('Error fetching dashboard data:', error);
         setIsLoading(false);
       });
-}, []);
+  }, []);
 
   if (isLoading) {
     return <LoadingScreen />;
   }
+  // If you are annoyed about the warnings in the console just comment out the sensor-card component
   return (
     <div className="p-4">
       <div className='flex flex-row justify-between'>
-      <Name_card greenhouseData= {greenhouseData}/>
-      <ClockCard />
+        <Name_card greenhouseData={greenhouseData} />
+        <ClockCard />
       </div>
-      <Sensor_card lightSensorData={lightSensorData} temperatureSensorData={temperatureSensorData} humiditySensorData={humiditySensorData} soilMoistureSensorData={soilMoistureSensorData}/>
+      <Sensor_card lightSensorData={lightSensorData}
+        temperatureSensorData={temperatureSensorData}
+        humiditySensorData={humiditySensorData}
+        soilMoistureSensorData={soilMoistureSensorData}
+        lightSensorDataAverageToday={lightSensorDataAverageToday}
+        temperatureSensorDataAverageToday={temperatureSensorDataAverageToday}
+        humiditySensorDataAverageToday={humiditySensorDataAverageToday}
+        soilMoistureSensorDataAverageToday={soilMoistureSensorDataAverageToday}
+        lightSensorDataAverageYesterday={lightSensorDataAverageYesterday}
+        temperatureSensorDataAverageYesterday={temperatureSensorDataAverageYesterday}
+        humiditySensorDataAverageYesterday={humiditySensorDataAverageYesterday}
+        soilMoistureSensorDataAverageYesterday={soilMoistureSensorDataAverageYesterday} />
       <SensorOverview />
       <div className="flex ">
-      <Notification_centre />
-      <AIModelPredictions metrics={_dummyMetrics} />
+        <Notification_centre />
+        <AIModelPredictions metrics={_dummyMetrics} />
       </div>
     </div>
   );

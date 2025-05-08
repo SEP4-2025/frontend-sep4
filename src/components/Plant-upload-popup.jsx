18c81eb821b86088f5dfe7906eb1a847d618ec50
darioml22 <darioml225@gmail.com>
2025-05-08 15:26:20 +0200

    Gallery page responsiveness fix

--- src/components/Plant-upload-popup.jsx
+++ src/components/Plant-upload-popup.jsx
@@ -8,24 +8,24 @@ function Plant_upload_popup ({isOpen, onClose}) {
     if (!isOpen) return null;
 
     return (
-        <div className="flex flex-col w-100 position:fixed inset-0 z-50">
-        <div className="flex flex-col items-center gap-4 bg-white p-6 rounded-xl shadow-lg">
-          <img src={logo} alt="logo" className="w-full max-w-xs h-auto block mx-auto" />
-          <p>Drag and upload</p>
-          <div className='flex flex-col w-full gap-2'>
-            <p>Write a note!</p>
-            <input
-              type="text"
-              id="note"
-              className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
-              placeholder="Add a note..."
-            />
-            <button className="bg-[#D5A632] hover:bg-black text-white py-2 px-4 rounded-xl">
-              Upload plant photo
-            </button>
-            <button onClick={onClose} className="text-red-500">Close</button>
-          </div>
+      <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-opacity-30 max-h-screen overflow-y-auto">
+      <div className="w-full max-w-md bg-white p-6 rounded-xl shadow-lg flex flex-col items-center gap-4">
+        <img src={logo} alt="logo" className="w-full max-w-xs h-auto block mx-auto" />
+        <p>Drag and upload</p>
+        <div className="flex flex-col w-full gap-2">
+          <p>Write a note!</p>
+          <input
+            type="text"
+            id="note"
+            className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
+            placeholder="Add a note..."
+          />
+          <button className="bg-[#D5A632] hover:bg-black text-white py-2 px-4 rounded-xl">
+            Upload plant photo
+          </button>
+          <button onClick={onClose} className="text-red-500">Close</button>
         </div>
+      </div>
     </div>
     )
 }
--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -90,7 +90,7 @@ function GalleryPage() {
                         )}
                     </div>
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {filteredPlants.length > 0 ? (
                     filteredPlants.map(plant => (
                         <div key={plant.id} onClick={() => setSelectedPlant(plant)}>

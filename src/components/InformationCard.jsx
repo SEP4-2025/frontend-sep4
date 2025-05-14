e92b9831965581eb33a45c9ab4d82538d04e9d0c
Richardzik <kubrama04@gmail.com>
2025-05-14 17:26:12 +0200

    Fixed minor issues with about us page

--- src/components/InformationCard.jsx
+++ src/components/InformationCard.jsx
@@ -1,7 +1,7 @@
 import { useDarkMode } from '../context/DarkModeContext';
 import informationCardPicture from '../assets/information-card-picture.svg';
 
-function LoadingScreen() {
+function InformationCard() {
     const { darkMode } = useDarkMode();
 
     return (
@@ -9,7 +9,7 @@ function LoadingScreen() {
             <div className={`flex flex-row items-center p-3 border rounded-lg ${darkMode ? 'border-gray-700 bg-slate-600' : 'border-gray-300 bg-gray-50'}`}>
                 <div className="flex flex-col gap-6 p-3">
                     <h1 className='Jacques-Francois text-3xl font-bold '>Who we are? </h1>
-                    <p className='Manrope text-l'>At GrowMate, we are passionate innovators dedicated to revolutionizing home gardening through smart technology. 
+                    <p className='Manrope text-base'>At GrowMate, we are passionate innovators dedicated to revolutionizing home gardening through smart technology. 
                     Our team of engineers, developers, and plant enthusiasts has created a comprehensive smart greenhouse solution 
                     that makes growing plants at home effortless and sustainable. We combine cutting-edge IoT sensors, intuitive 
                     software, and ecological principles to help you nurture thriving plants while conserving resources. Our mission 
@@ -22,4 +22,4 @@ function LoadingScreen() {
     );
 }
 
-export default LoadingScreen;
+export default InformationCard;

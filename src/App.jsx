1da772f252354dd60004c67c1ed7c47ac0467cfd
Mateusz Samborski <msamborski04@gmail.com>
2025-05-19 17:20:20 +0200

    initial test for gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import { useState } from 'react';
 
 // Components
